759d86d573bd1e6aa8d99e350e3b26ba915cf0ab
unknown <Jhanmaicol@PC-6.LABSTI.USMP>
2018-02-02 15:11:06 -0500

    demo juego en red

---
+++ 3er Persona/Assets/NetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Photon.MonoBehaviour {
+
+    public string Version;
+	
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(Version);
+	}
+
+    void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Global", new RoomOptions() { maxPlayers = 30 }, null);
+    }
+	
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Personaje3d", transform.position, transform.rotation, 0);
+    }
+}
---
+++ 3er Persona/Assets/Scripts/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : Photon.MonoBehaviour {
+
+    public Animator Anim;
+    public float WalkSpeed;
+
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (photonView.isMine)
+            {
+                Anim.SetBool("Walk", true);
+                transform.Translate(Vector3.forward * WalkSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            
+                Anim.SetBool("Walk", false);
+            
+        }        
+        
+    }
+   /* void FixedUpdate () {
+
+         float h = Input.GetAxis("Vertical");
+
+         if (photonView.isMine)
+         {
+
+            
+            Anim.SetFloat("Walk", h);
+         }
+
+     } */
+}
